22376d6628b5ae5be1f57227a4908bb34b327bd6
Oliver Juhas <webmandesign@users.noreply.github.com>
2016-07-27 08:03:41 +0200

    Reformatting the code

--- use-child-theme.php
+++ use-child-theme.php
@@ -22,193 +22,230 @@ defined( 'ABSPATH' ) or exit;
 
 if ( ! class_exists( 'Use_Child_Theme' ) && is_admin() ) {
 
-    class Use_Child_Theme
-    {
-
-        public $theme;
-        public $child_slug;
+	class Use_Child_Theme {
 
 
-        function __construct() {
-            add_action( 'admin_init', array( $this, 'admin_init' ) );
-        }
 
 
-        function admin_init() {
 
-            // Exit if unauthorized
-            if ( ! current_user_can( 'switch_themes' ) ) {
-                return;
-            }
-
-            // Exit if dismissed
-            if ( false !== get_transient( 'uct_dismiss_notice' ) ) {
-                return;
-            }
-
-            $this->theme = wp_get_theme();
-
-            // Exit if child theme
-            if ( false !== $this->theme->parent() ) {
-                return;
-            }
-
-            // Exit if no direct access
-            if ( 'direct' != get_filesystem_method() ) {
-                return;
-            }
-
-            add_action( 'wp_ajax_uct_activate', array( $this, 'activate_child_theme' ) );
-            add_action( 'wp_ajax_uct_dismiss', array( $this, 'dismiss_notice' ) );
-            add_action( 'admin_notices', array( $this, 'admin_notices' ) );
-        }
+		public $theme;
+		public $child_slug;
 
 
-        function admin_notices() {
-
-            // Show only on specific admin page(s) (default: Appearance > Editor)
-            $admin_notices_screen_id = apply_filters( 'uct_admin_notices_screen_id', array( 'theme-editor' ) );
-            $screen = get_current_screen();
-            if ( ! isset( $screen->id ) || ! in_array( $screen->id, (array) $admin_notices_screen_id ) ) {
-                return;
-            }
-            
-            ?>
-            <script>
-            (function($) {
-                $(function() {
-                    $(document).on('click', '.uct-activate', function() {
-                        $.post(ajaxurl, { action: 'uct_activate' }, function(response) {
-                            $('.uct-notice p').html(response);
-                        });
-                    });
-    
-                    $(document).on('click', '.uct-notice .notice-dismiss', function() {
-                        $.post(ajaxurl, { action: 'uct_dismiss' });
-                    });
-                });
-            })(jQuery);
-            </script>
-    
-            <div class="notice notice-error uct-notice is-dismissible">
-                <p>
-                    <?php printf( esc_html__( 'Please use a %s child theme to make changes!', '{%= text_domain %}' ), $this->theme->get( 'Name' ) ); ?>
-                    <a class="uct-activate" href="javascript:;"><?php esc_html_e( 'Activate now &raquo;', '{%= text_domain %}' ); ?></a>
-                </p>
-            </div>
-            <?php
-        }
 
 
-        function dismiss_notice() {
-            set_transient( 'uct_dismiss_notice', 'yes', apply_filters( 'uct_dismiss_timeout', 86400 ) );
-            exit;
-        }
+
+		function __construct() {
+
+			add_action( 'admin_init', array( $this, 'admin_init' ) );
+
+		} // /__construct
 
 
-        function has_child_theme() {
-            $themes = wp_get_themes();
-            $folder_name = $this->theme->get_stylesheet();
-            $this->child_slug = $folder_name . '-child';
 
-            foreach ( $themes as $theme ) {
-                if ( $folder_name == $theme->get( 'Template' ) ) {
-                    $this->child_slug = $theme->get_stylesheet();
-                    return true;
-                }
-            }
+		function admin_init() {
 
-            return false;
-        }
+			// Exit if unauthorized
+			if ( ! current_user_can( 'switch_themes' ) ) {
+				return;
+			}
 
+			// Exit if dismissed
+			if ( false !== get_transient( 'uct_dismiss_notice' ) ) {
+				return;
+			}
 
-        function activate_child_theme() {
-            $parent_slug = $this->theme->get_stylesheet();
+			$this->theme = wp_get_theme();
 
-            // Create child theme
-            if ( ! $this->has_child_theme() ) {
-                $this->create_child_theme();
-            }
+			// Exit if child theme
+			if ( false !== $this->theme->parent() ) {
+				return;
+			}
 
-            switch_theme( $this->child_slug );
+			// Exit if no direct access
+			if ( 'direct' != get_filesystem_method() ) {
+				return;
+			}
 
-            // Copy customizer settings, widgets, etc.
-            $settings = get_option( 'theme_mods_' . $this->child_slug );
-
-            if ( false === $settings ) {
-                $parent_settings = get_option( 'theme_mods_' . $parent_slug );
-                update_option( 'theme_mods_' . $this->child_slug, $parent_settings );
-            }
-
-            wp_die( esc_html__( 'All done! You are using a child theme now!', '{%= text_domain %}' ) );
-        }
+			add_action( 'wp_ajax_uct_activate', array( $this, 'activate_child_theme' ) );
+			add_action( 'wp_ajax_uct_dismiss', array( $this, 'dismiss_notice' ) );
+			add_action( 'admin_notices', array( $this, 'admin_notices' ) );
+		
+		} // /admin_init
 
 
-        function create_child_theme() {
-            $parent_dir = $this->theme->get_stylesheet_directory();
-            $child_dir = $parent_dir . '-child';
 
-            if ( wp_mkdir_p( $child_dir ) ) {
-                $creds = request_filesystem_credentials( admin_url() );
-                WP_Filesystem( $creds ); // we already have direct access
+		function admin_notices() {
 
-                global $wp_filesystem;
-                $wp_filesystem->put_contents( $child_dir . '/style.css', $this->style_css() );
-                $wp_filesystem->put_contents( $child_dir . '/functions.php', $this->functions_php() );
+			// Show only on specific admin page(s) (default: Appearance > Editor)
+			$admin_notices_screen_id = apply_filters( 'uct_admin_notices_screen_id', array( 'theme-editor' ) );
+			$screen = get_current_screen();
+			if ( ! isset( $screen->id ) || ! in_array( $screen->id, (array) $admin_notices_screen_id ) ) {
+				return;
+			}
+			
+			?>
 
-                if ( false !== ( $img = $this->theme->get_screenshot( 'relative' ) ) ) {
-                    $wp_filesystem->copy( "$parent_dir/$img", "$child_dir/$img" );
-                }
-            }
-            else {
-                wp_die( esc_html__( 'Error: theme folder not writable!', '{%= text_domain %}' ) );
-            }
-        }
+			<script>
+			(function($) {
+				$(function() {
+					$(document).on('click', '.uct-activate', function() {
+						$.post(ajaxurl, { action: 'uct_activate' }, function(response) {
+							$('.uct-notice p').html(response);
+						});
+					});
+	
+					$(document).on('click', '.uct-notice .notice-dismiss', function() {
+						$.post(ajaxurl, { action: 'uct_dismiss' });
+					});
+				});
+			})(jQuery);
+			</script>
+	
+			<div class="notice notice-error uct-notice is-dismissible">
+				<p>
+					<?php printf( esc_html__( 'Please use a %s child theme to make changes!', '{%= text_domain %}' ), $this->theme->get( 'Name' ) ); ?>
+					<a class="uct-activate" href="javascript:;"><?php esc_html_e( 'Activate now &raquo;', '{%= text_domain %}' ); ?></a>
+				</p>
+			</div>
+
+			<?php
+
+		} // /admin_notices
 
 
-        function style_css() {
-            $output  = '/**' . "\r\n";
-            $output .= ' * Theme Name: ' . $this->theme->get( 'Name' ) . ' Child' . "\r\n";
-            $output .= ' * Template: ' . $this->theme->get_stylesheet() . "\r\n";
-            $output .= ' * Version: 1.0' . "\r\n";
-            $output .= ' * Description: This is a child theme of ' . $this->theme->get( 'Name' ) . "\r\n";
-            $output .= ' */' . "\r\n";
-            $output .= "\r\n";
-            $output .= "\r\n";
-            $output .= "\r\n";
-            $output .= '/* Put your custom CSS styles below... */' . "\r\n";
-            
-            return apply_filters( 'uct_style_css', $output );
-        }
+
+		function dismiss_notice() {
+
+			set_transient( 'uct_dismiss_notice', 'yes', apply_filters( 'uct_dismiss_timeout', 86400 ) );
+			exit;
+
+		} // /dismiss_notice
 
 
-        function functions_php() {
-            $output  = '/**' . "\r\n";
-            $output .= ' * Child theme functions' . "\r\n";
-            $output .= ' *' . "\r\n";
-            $output .= ' * @package  ' . $this->theme->get( 'Name' ) . ' Child' . "\r\n";
-            $output .= ' */' . "\r\n";
-            $output .= "\r\n";
-            $output .= "\r\n";
-            $output .= "\r\n";
-            $output .= '/**' . "\r\n";
-            $output .= ' * Enqueue parent theme stylesheet the right way' . "\r\n";
-            $output .= ' */' . "\r\n";
-            $output .= 'function child_theme_enqueue_parent_styles() {' . "\r\n";
-            $output .= "\t" . "if ( current_theme_supports( 'child-theme-stylesheet' ) ) { return; }" . "\r\n";
-            $output .= "\t" . "wp_enqueue_style( 'parent-theme-styles', get_template_directory_uri() . '/style.css' );" . "\r\n";
-            $output .= "\t" . "wp_enqueue_style( 'child-theme-styles', get_stylesheet_uri() );" . "\r\n";
-            $output .= '}' . "\r\n";
-            $output .= "\r\n";
-            $output .= "add_action( 'wp_enqueue_scripts', 'child_theme_enqueue_parent_styles', 1000 );\r\n";
-            $output .= "\r\n";
-            $output .= "\r\n";
-            $output .= "\r\n";
-            $output .= '/* Put your custom PHP code below... */' . "\r\n";
-            
-            return apply_filters( 'uct_functions_php', $output );
-        }
-    }
 
-    new Use_Child_Theme();
+		function has_child_theme() {
+
+			$themes = wp_get_themes();
+			$folder_name = $this->theme->get_stylesheet();
+			$this->child_slug = $folder_name . '-child';
+
+			foreach ( $themes as $theme ) {
+				if ( $folder_name == $theme->get( 'Template' ) ) {
+					$this->child_slug = $theme->get_stylesheet();
+					return true;
+				}
+			}
+
+			return false;
+		
+		} // /has_child_theme
+
+
+
+		function activate_child_theme() {
+
+			$parent_slug = $this->theme->get_stylesheet();
+
+			// Create child theme
+			if ( ! $this->has_child_theme() ) {
+				$this->create_child_theme();
+			}
+
+			switch_theme( $this->child_slug );
+
+			// Copy customizer settings, widgets, etc.
+			$settings = get_option( 'theme_mods_' . $this->child_slug );
+
+			if ( false === $settings ) {
+				$parent_settings = get_option( 'theme_mods_' . $parent_slug );
+				update_option( 'theme_mods_' . $this->child_slug, $parent_settings );
+			}
+
+			wp_die( esc_html__( 'All done! You are using a child theme now!', '{%= text_domain %}' ) );
+		
+		} // /activate_child_theme
+
+
+
+		function create_child_theme() {
+
+			$parent_dir = $this->theme->get_stylesheet_directory();
+			$child_dir = $parent_dir . '-child';
+
+			if ( wp_mkdir_p( $child_dir ) ) {
+				$creds = request_filesystem_credentials( admin_url() );
+				WP_Filesystem( $creds ); // we already have direct access
+
+				global $wp_filesystem;
+				$wp_filesystem->put_contents( $child_dir . '/style.css', $this->style_css() );
+				$wp_filesystem->put_contents( $child_dir . '/functions.php', $this->functions_php() );
+
+				if ( false !== ( $img = $this->theme->get_screenshot( 'relative' ) ) ) {
+					$wp_filesystem->copy( "$parent_dir/$img", "$child_dir/$img" );
+				}
+			} else {
+				wp_die( esc_html__( 'Error: theme folder not writable!', '{%= text_domain %}' ) );
+			}
+
+		} // /create_child_theme
+
+
+
+		function style_css() {
+		
+			$output  = '/**' . "\r\n";
+			$output .= ' * Theme Name: ' . $this->theme->get( 'Name' ) . ' Child' . "\r\n";
+			$output .= ' * Template: ' . $this->theme->get_stylesheet() . "\r\n";
+			$output .= ' * Version: 1.0' . "\r\n";
+			$output .= ' * Description: This is a child theme of ' . $this->theme->get( 'Name' ) . "\r\n";
+			$output .= ' */' . "\r\n";
+			$output .= "\r\n";
+			$output .= "\r\n";
+			$output .= "\r\n";
+			$output .= '/* Put your custom CSS styles below... */' . "\r\n";
+			
+			return apply_filters( 'uct_style_css', $output );
+		
+		} // /style_css
+
+
+
+		function functions_php() {
+
+			$output  = '/**' . "\r\n";
+			$output .= ' * Child theme functions' . "\r\n";
+			$output .= ' *' . "\r\n";
+			$output .= ' * @package  ' . $this->theme->get( 'Name' ) . ' Child' . "\r\n";
+			$output .= ' */' . "\r\n";
+			$output .= "\r\n";
+			$output .= "\r\n";
+			$output .= "\r\n";
+			$output .= '/**' . "\r\n";
+			$output .= ' * Enqueue parent theme stylesheet the right way' . "\r\n";
+			$output .= ' */' . "\r\n";
+			$output .= 'function child_theme_enqueue_parent_styles() {' . "\r\n";
+			$output .= "\t" . "if ( current_theme_supports( 'child-theme-stylesheet' ) ) { return; }" . "\r\n";
+			$output .= "\t" . "wp_enqueue_style( 'parent-theme-styles', get_template_directory_uri() . '/style.css' );" . "\r\n";
+			$output .= "\t" . "wp_enqueue_style( 'child-theme-styles', get_stylesheet_uri() );" . "\r\n";
+			$output .= '}' . "\r\n";
+			$output .= "\r\n";
+			$output .= "add_action( 'wp_enqueue_scripts', 'child_theme_enqueue_parent_styles', 1000 );\r\n";
+			$output .= "\r\n";
+			$output .= "\r\n";
+			$output .= "\r\n";
+			$output .= '/* Put your custom PHP code below... */' . "\r\n";
+			
+			return apply_filters( 'uct_functions_php', $output );
+		
+		} // /functions_php
+
+
+
+
+
+	} // /Use_Child_Theme
+
+	new Use_Child_Theme();
+
 }
